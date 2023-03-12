f6578fe311335977c84d94bc20bf559eeb7c665f
RonD101 <ron.dahan@campus.technion.ac.il>
2023-03-12 13:13:30 +0200

    Extension now work on open tab

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,20 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Created IncludeLess extension.');
 	// Called everytime extension is used
 	let disposable = vscode.commands.registerCommand('includeless.includeless', async () => {
-		let source_file_path = path.join(__dirname, "../src/tests/simple/main.cpp");
+		let source_file_path : string;
+		if (vscode.window.activeTextEditor !== undefined) {
+			source_file_path = vscode.window.activeTextEditor.document.fileName;
+			var fileExt = path.basename(source_file_path).split('.').pop();
+			if (fileExt !== 'c' && fileExt !== 'cpp' && fileExt !== 'h' && fileExt !== 'cxx') {
+				vscode.window.showInformationMessage('Extension can only be used on source files and headers');
+				return;
+			}
+			
+		} else {
+			vscode.window.showInformationMessage('Open a source file to use the extension');
+			return;
+		}
+		// let source_file_path = path.join(__dirname, "../src/tests/simple/main.cpp");
 		let exe_file_path = path.join(__dirname, "../src/backend/main.exe");
 		const execPromise = new Promise((resolve, reject) => {
 			exec(exe_file_path, [source_file_path], (error: any, stdout: any, stderr: any) => {
@@ -34,13 +47,20 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function getRanges(): Array<vscode.Range> {
-	const ranges_path = path.join(__dirname, "../src/tests/simple/ranges.txt");
+	let current_working_dir : string;
+		if (vscode.window.activeTextEditor !== undefined) {
+			current_working_dir = path.dirname(vscode.window.activeTextEditor.document.fileName);
+		} else {
+			vscode.window.showInformationMessage('Couldn\'t find ranges');
+			return [];
+		}
+	const ranges_path = path.join(current_working_dir, "/ranges.txt");
 	const ranges_file = readFileSync(ranges_path, 'utf-8');
 	const lines = ranges_file.split('\n');
 	let ranges = [];
 	for (var line of lines) {
 		const sub_range = line.split(' ');
-		if (sub_range.length === 0) {
+		if (sub_range.length === 0 || sub_range.length === 1) {
 			continue;
 		}
 		if (sub_range.length !== 3) {
@@ -59,7 +79,6 @@ function updateDiagnostics(document: vscode.TextDocument, collection: vscode.Dia
 	let ranges = getRanges();
 	let warn_ranges = [];
 	for (var i_range of ranges) {
-		console.log(i_range);
 		let warn = new vscode.Diagnostic(i_range, 'You can remove this Include', vscode.DiagnosticSeverity.Warning);
 		warn_ranges.push(warn);
 	}
