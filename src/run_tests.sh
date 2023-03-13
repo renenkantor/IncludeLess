918c7db842050d8ba41e17a36fb16fbbd5b7f7b7
RonD101 <ron.dahan@campus.technion.ac.il>
2023-03-13 12:24:19 +0200

    Update test script to produce less information on console

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -3,16 +3,28 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 cd backend
-make
-echo "Finished build"
+
 pwd_v=$(pwd)
 
 pwd_test="$pwd_v/../tests/"
-echo $pwd_test
+output_path="$pwd_v/../run_test_output.txt"
+
+make &> "$output_path"
+echo "Finished build"
+echo $pwd_test >> "$output_path" 2>&1
 for file in $pwd_test*
 do
-  echo "Running $file/main.cpp"
+  echo "Running $file/main.cpp" >> "$output_path" 2>&1
   ./main.exe "$file/main.cpp" &> /dev/null
-  diff "$file/useless_includes_ref.txt" "$file/useless_includes.txt"
-  diff "$file/all_includes_ref.txt" "$file/all_includes.txt"
+  if [[ -e "$file/useless_includes_ref.txt" && -e "$file/useless_includes.txt" ]]; then
+    if ! diff "$file/useless_includes_ref.txt" "$file/useless_includes.txt" >> "$output_path" 2>&1; then
+      echo "Diff failed for file $file/useless_includes.txt"
+    fi
+  fi
+  if [[ -e "$file/all_includes_ref.txt" && -e "$file/all_includes.txt" ]]; then
+    if ! diff "$file/all_includes_ref.txt" "$file/all_includes.txt" >> "$output_path" 2>&1; then
+      echo "Diff failed for file $file/all_includes.txt"
+    fi
+  fi
 done
+echo "Finished tests"
